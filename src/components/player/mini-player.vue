24bab621d7743b5325397c1cf1942de1ffa8ea9a
xiaogang12138 <110959135+xiaogang12138@users.noreply.github.com>
2022-09-09 17:53:01 +0800

    Update mini-player.vue

--- src/components/player/mini-player.vue
+++ src/components/player/mini-player.vue
@@ -3,119 +3,30 @@
     <div
       class="mini-player"
       v-show="!fullScreen"
-      @click="showNormalPlayer"
     >
       <div class="cd-wrapper">
         <div
-          ref="cdRef"
           class="cd"
         >
           <img
-            ref="cdImageRef"
             width="40"
             height="40"
             :src="currentSong.pic"
-            :class="cdCls"
           >
         </div>
       </div>
-      <div
-        ref="sliderWrapperRef"
-        class="slider-wrapper"
-      >
-        <div class="slider-group">
-          <div
-            class="slider-page"
-            v-for="song in playlist"
-            :key="song.id"
-          >
-            <h2 class="name">{{song.name}}</h2>
-            <p class="desc">{{song.singer}}</p>
-          </div>
-        </div>
+      <div>
+        <h2 class="name">{{currentSong.name}}</h2>
+        <p class="desc">{{currentSong.singer}}</p>
       </div>
-      <div class="control">
-        <progress-circle
-          :radius="32"
-          :progress="progress"
-        >
-          <i
-            class="icon-mini"
-            :class="miniPlayIcon"
-            @click.stop="togglePlay"
-          ></i>
-        </progress-circle>
       </div>
-      <div class="control" @click.stop="showPlaylist">
-        <i class="icon-playlist"></i>
-      </div>
-      <playlist ref="playlistRef"></playlist>
-    </div>
   </transition>
 </template>
 
 <script>
-  import { useStore } from 'vuex'
-  import { computed, ref } from 'vue'
-  import useCd from './use-cd'
-  import useMiniSlider from './use-mini-slider'
-  import ProgressCircle from './progress-circle'
-  import Playlist from './playlist'
-
-  export default {
-    name: 'mini-player',
-    components: {
-      Playlist,
-      ProgressCircle
-    },
-    props: {
-      progress: {
-        type: Number,
-        default: 0
-      },
-      togglePlay: Function
-    },
-    setup() {
-      const playlistRef = ref(null)
-
-      const store = useStore()
-      const fullScreen = computed(() => store.state.fullScreen)
-      const currentSong = computed(() => store.getters.currentSong)
-      const playing = computed(() => store.state.playing)
-      const playlist = computed(() => store.state.playlist)
-
-      const { cdCls, cdRef, cdImageRef } = useCd()
-      const { sliderWrapperRef } = useMiniSlider()
-
-      const miniPlayIcon = computed(() => {
-        return playing.value ? 'icon-pause-mini' : 'icon-play-mini'
-      })
-
-      function showNormalPlayer() {
-        store.commit('setFullScreen', true)
-      }
-
-      function showPlaylist() {
-        playlistRef.value.show()
-      }
-
-      return {
-        playlistRef,
-        fullScreen,
-        currentSong,
-        playlist,
-        miniPlayIcon,
-        showNormalPlayer,
-        showPlaylist,
-        // cd
-        cdCls,
-        cdRef,
-        cdImageRef,
-        // mini-slider
-        sliderWrapperRef
-      }
-    }
-  }
+ export default {
+  
+ }
 </script>
 
 <style lang="scss" scoped>
